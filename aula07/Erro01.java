53880f26e692e4eb4cc119de298753e810413814
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-05 18:11:25 -0300

    errorsHandling

---
+++ aula07/Erro01.java
@@ -0,0 +1,7 @@
+class Erro01 {
+	// Simulado erro de sintaxe
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+}
---
+++ aula07/Erro03.java
@@ -0,0 +1,7 @@
+class Erro03 {
+	// Simulado erro de não ocorre em tempo de execução
+	public static void main(String[] args) {
+
+		System.out.println("Você digitou: ");
+	}
+}

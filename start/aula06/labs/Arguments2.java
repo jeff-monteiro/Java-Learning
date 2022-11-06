cffe86cc8505b897e57bb3dc0ad10dfa16419ec3
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-05 21:54:04 -0300

    settingUpFiles

---
+++ start/aula06/labs/Arguments2.java
@@ -0,0 +1,5 @@
+class Arguments2 {
+	public static void main(String[] args) {
+		System.out.println("Hello World! " + args[1]);
+	}
+}
---
+++ start/aula07/Erro02.java
@@ -0,0 +1,9 @@
+class Erro02 {
+	// Simulado erro de semântica
+	public static void main(String[] args) {
+
+		int cont = 0;
+		
+		System.out.println("Você digitou: " + cont);
+	}
+}

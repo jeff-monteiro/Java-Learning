67ce6bc1619897bcc58928aa3bdc0a24b4d55157
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-06 12:01:27 -0300

    typeVariables

---
+++ javaEngineer/src/javaEngineer/aula11/CharVariables.java
@@ -0,0 +1,15 @@
+package javaEngineer.aula11;
+
+public class CharVariables {
+
+	public static void main(String[] args) {
+		
+		char genderMale = 'M';
+		
+		char genderFemale = 'F';
+		
+		System.out.println("That's your gender: " + genderFemale);
+		System.out.println("That's your gender: " + genderMale);
+	}
+
+}

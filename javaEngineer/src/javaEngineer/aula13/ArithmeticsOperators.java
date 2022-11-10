cda7acf92ca41572653fb05a66ce0276df0ecf70
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-09 23:39:48 -0300

    arithmeticsOperators

---
+++ javaEngineer/src/javaEngineer/aula13/ArithmeticsOperators.java
@@ -0,0 +1,27 @@
+package javaEngineer.aula13;
+
+public class ArithmeticsOperators {
+
+	public static void main(String[] args) {
+		
+		int resultado = 1 + 4;
+		System.out.println(resultado);
+		
+		resultado = resultado - 1;
+		System.out.println(resultado);
+		
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		
+		resultado = resultado * 3;
+		System.out.println(resultado);
+		
+		resultado = resultado + 1;
+		System.out.println(resultado);
+		
+		resultado = resultado % 7;
+		System.out.println(resultado);
+
+	}
+
+}

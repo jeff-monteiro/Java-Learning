7b6f98f068bbfd383f03cc9a43a40358daffbd49
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-13 16:34:53 -0300

    exercisingFundamentals

---
+++ javaEngineer/src/javaEngineer/aula13labs/Exer09.java
@@ -0,0 +1,18 @@
+package javaEngineer.aula13labs;
+
+import java.util.Scanner;
+
+public class Exer09 {
+
+	public static void main(String[] args) {
+		
+		Scanner data = new Scanner(System.in);
+		
+		System.out.println("Insert the temperature in Farenheit: ");
+		double f = data.nextDouble();
+		
+		double c = (5 * (f - 32) / 9);
+		System.out.println("The temperature in Celsius is: " + c + "°F");
+	}
+
+}

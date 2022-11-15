c09f1895bc144c851a32f8c7256b2eb60f6b66d4
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-14 23:17:27 -0300

    exercisingFundamentals

--- javaEngineer/src/javaEngineer/aula13labs/Exer11.java
+++ javaEngineer/src/javaEngineer/aula13labs/Exer11.java
@@ -1,9 +1,22 @@
 package javaEngineer.aula13labs;
 
+import java.util.Scanner;
+
 public class Exer11 {
 
 	public static void main(String[] args) {
 		
+		Scanner data = new Scanner(System.in);
+		
+		System.out.println("Insert the integer: ");
+		int fstInt = data.nextInt();
+		System.out.println("Insert the second integer: ");
+		int scdInt = data.nextInt();
+		System.out.println("Insert the real number: ");
+		double trdReal = data.nextDouble();
+		
+		double resultOne = (2 * fstInt) + (scdInt / 2);
+		System.out.println("O product the first operation is: " + resultOne);
 		
 
 	}

d9a5633745bccf257ce68acc0be6e592b23790da
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-15 18:10:46 -0300

    exercisingFundamentals

---
+++ javaEngineer/src/javaEngineer/aula15labs/Exer02.java
@@ -0,0 +1,19 @@
+package javaEngineer.aula15labs;
+
+import java.util.Scanner;
+
+public class Exer02 {
+	public static void main(String[] args) {
+		
+		Scanner data = new Scanner(System.in);
+		
+		System.out.println("Enter with the value");
+		int num = data.nextInt();
+		
+		if(num >= 0) {
+			System.out.println("Number positive!");
+		}else {
+			System.out.println("Number negative");
+		}
+	}
+}
---
+++ javaEngineer/src/javaEngineer/aula15labs/Exer04.java
@@ -0,0 +1,38 @@
+package javaEngineer.aula15labs;
+
+import java.util.Scanner;
+
+public class Exer04 {
+
+	public static void main(String[] args) {
+		
+		Scanner data = new Scanner(System.in);
+		
+		System.out.println("Enter with a letter: ");
+		String letter = data.next();
+		
+		if(letter.length() > 1) {
+			System.out.println("It's not a valid letter");
+		}else {
+			switch(letter) {
+			case "a":
+			case "e":
+			case "i":
+			case "o":
+			case "u":
+			case "A":
+			case "E":
+			case "I":
+			case "O":
+			case "U":
+				System.out.println("It's a vowel");
+				break;
+			default:
+				System.out.println("It's a consonant");
+			}
+
+		}
+		
+	}
+
+}

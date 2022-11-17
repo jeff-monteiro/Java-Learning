9beb1fb93209418c46913bb22405ac9b4e24e029
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-16 23:05:39 -0300

    exercisingFundamentals

--- javaEngineer/src/javaEngineer/aula17/LoopFor.java
+++ javaEngineer/src/javaEngineer/aula17/LoopFor.java
@@ -7,6 +7,10 @@ public class LoopFor {
 		for( int i = 0; i <= 10; i++) {
 			System.out.println("The value of the i is: " + i);
 		}
+		
+		for( int j = 10; j >= 0; j--) {
+			System.out.println("The value of the j is: " + j);
+		}
 
 	}
 

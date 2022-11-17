1c308938057aa724c9d50c51d6cde734ba0e3c8a
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-16 22:55:35 -0300

    exercisingFundamentals

---
+++ javaEngineer/src/javaEngineer/aula17/LoopFor.java
@@ -0,0 +1,13 @@
+package javaEngineer.aula17;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+		
+		for( int i = 0; i <= 10; i++) {
+			System.out.println("The value of the i is: " + i);
+		}
+
+	}
+
+}

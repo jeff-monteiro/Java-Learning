118a1ee974830e8e83e7c2c9155b9bc36de42211
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-17 23:55:04 -0300

    exercisingFundamentals

---
+++ javaEngineer/src/javaEngineer/aula18/BreakAndContinue.java
@@ -0,0 +1,17 @@
+package javaEngineer.aula18;
+
+import java.util.Scanner;
+
+public class BreakAndContinue {
+
+	public static void main(String[] args) {
+		
+		Scanner data = new Scanner(System.in);
+		
+		System.out.println("Enter with a number: ");
+		int number = data.nextInt();
+		
+		System.out.println("Enter with a limit");
+	}
+
+}

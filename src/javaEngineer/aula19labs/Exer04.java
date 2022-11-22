c0c12c3f1951bc32a8d44fe65ff1ba68fcc43c38
Jefferson <jefferson.moura@hp.com>
2022-11-22 20:07:07 -0300

    updating exercising four

--- src/javaEngineer/aula19labs/Exer04.java
+++ src/javaEngineer/aula19labs/Exer04.java
@@ -1,7 +1,15 @@
 package src.javaEngineer.aula19labs;
 
+import java.util.Scanner;
+
 public class Exer04 {
     public static void main(String[] args) {
-        
+
+        Scanner data = new Scanner(System.in);
+
+        int number = data.nextInt();
+        for(int i=0; i<number; i++){
+            System.out.println(i);
+        }
     }
 }

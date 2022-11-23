21d40a77ac0739a3878579d3be5db46854037375
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-23 08:17:24 -0300

    exercising fundamentals

--- src/javaEngineer/aula19labs/Exer05.java
+++ src/javaEngineer/aula19labs/Exer05.java
@@ -3,18 +3,18 @@ package src.javaEngineer.aula19labs;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class Exer04 {
+public class Exer05 {
     public static void main(String[] args) {
 
         Scanner data = new Scanner(System.in);
 
-        int[] vetorA = new int[15];
-        double[] vetorB = new double[vetorA.length];
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
         for(int i=0; i<vetorA.length; i++){
             System.out.println("The position of i is: " + i);
             vetorA[i] = data.nextInt();
 
-            vetorB[i] = Math.sqrt(vetorA[i]);
+            vetorB[i] = vetorA[i] * i;
         }
         System.out.println("Insert the vetorA");
         for(int i=0; i<vetorA.length; i++){
@@ -22,12 +22,9 @@ public class Exer04 {
         }
         System.out.println();
 
-        // To format the numbers
-        DecimalFormat df = new DecimalFormat("###,###.###");
-
         System.out.println("Insert the vetorB");
         for(int i=0; i<vetorB.length; i++){
-            System.out.println(df.format(vetorB[i]));
+            System.out.println(vetorB[i]);
         }
         System.out.println();
     }

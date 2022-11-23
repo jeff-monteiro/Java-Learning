20ac9c7692a6f3a7fcf872f184d5b9296fab418c
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-22 23:46:04 -0300

    implemeting the use of Arrays

--- src/javaEngineer/aula19labs/Exer04.java
+++ src/javaEngineer/aula19labs/Exer04.java
@@ -7,9 +7,24 @@ public class Exer04 {
 
         Scanner data = new Scanner(System.in);
 
-        int number = data.nextInt();
-        for(int i=0; i<number; i++){
-            System.out.println("This is the value of: " + i);
+        int[] vetorA = new int[15];
+        double[] vetorB = new double[vetorA.length];
+        for(int i=0; i<vetorA.length; i++){
+            System.out.println("The position of i is: " + i);
+            vetorA[i] = data.nextInt();
+
+            vetorB[i] = Math.sqrt(vetorA[i]);
         }
+        System.out.println("Insert the vetorA");
+        for(int i=0; i<vetorA.length; i++){
+            System.out.println(vetorA[i]);
+        }
+        System.out.println();
+
+        System.out.println("Insert the vetorB");
+        for(int i=0; i<vetorB.length; i++){
+            System.out.println(vetorB[i]);
+        }
+        System.out.println();
     }
 }

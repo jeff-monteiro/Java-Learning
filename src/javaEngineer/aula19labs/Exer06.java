af91906b037bece74461152ab739df23c7fe6c21
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-25 23:47:59 -0300

    working on fundamentals

--- src/javaEngineer/aula19labs/Exer06.java
+++ src/javaEngineer/aula19labs/Exer06.java
@@ -16,5 +16,14 @@ public class Exer06 {
             vetA[i] = data.nextInt();
             vetB[i] = data.nextInt();
         }
+        System.out.println("The position of vetA ");
+        for(int i=0; i<vetA.length; i++){
+            System.out.println(vetA[i]);
+        }
+        System.out.println("The position of vetB ");
+        for(int i=0; i<vetB.length; i++){
+            System.out.println(vetB[i]);
+        }
+
     }
 }

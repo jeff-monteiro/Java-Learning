7eaf6a0ced05c91e02e7303a5b5c00edffee6695
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-27 13:20:52 -0300

    fundamentals

---
+++ src/CharactereTest.java
@@ -0,0 +1,21 @@
+package src;
+
+public class CharactereTest {
+    public static void main(String[] args) {
+
+        char letter = 'A';
+        System.out.println(letter);
+
+        char value = 66;
+        System.out.println(value);
+
+        value = (char) (value + 1);
+        System.out.println(value);
+
+        String word = "Alura courses online of technology";
+        System.out.println(word);
+
+        word = word + 2020;
+        System.out.println(word);
+    }
+}
---
+++ src/ConversionTest.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.Scanner;
+
+public class ConversionTest {
+    public static void main(String[] args) {
+
+        //Scanner data = new Scanner(System.in);
+        double salary = 1278.50;
+        int value = (int) salary;
+
+        System.out.println("The value will be showed is: " + value);
+
+        double value1 = 0.2;
+        double value2 = 0.1;
+        double total = value1 + value2;
+        System.out.println("The total is: " + total);
+    }
+}

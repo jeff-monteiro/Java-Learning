aa848e4db4539284aadb75a4d26625c6a1e21f68
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-11-27 23:34:54 -0300

    casting

---
+++ src/Testing.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Testing {
+    public static void main(String[] args) {
+
+        char letra = 'a';
+        System.out.println(letra);
+
+        char valor = 65;                    // Compila!
+        System.out.println(valor);
+
+        valor = (char) (valor + 1);         // Compila!
+        System.out.println(valor);
+
+        valor = (char) (valor + 2);
+        System.out.println(valor);
+    }
+
+}

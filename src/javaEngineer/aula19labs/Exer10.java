53e2568942b81b5aaf2d344ef293c1b1ea51546b
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-12-04 11:02:47 -0300

    exercising fundamentals

--- src/javaEngineer/aula19labs/Exer10.java
+++ src/javaEngineer/aula19labs/Exer10.java
@@ -7,6 +7,18 @@ public class Exer10 {
 
         Scanner data = new Scanner(System.in);
 
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
 
+        for(int i=0; i < vetorA.length; i++){
+            System.out.println("Insert the value of position at: " + i);
+
+            vetorA[i] = data.nextInt();
+            vetorB[i] = data.nextInt();
+        }
+        System.out.println("Enter with vetorA ");
+        for(int i=0; i < vetorA.length; i++){
+            System.out.println(vetorA[i]);
+        }
     }
 }

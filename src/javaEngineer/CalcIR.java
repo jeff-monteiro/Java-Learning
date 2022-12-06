0aaf0db4615611db0005043dfda76e4d51ef68db
Jefferson <jefferson.moura@hp.com>
2022-12-06 09:35:19 -0300

    IR Calculator

---
+++ src/javaEngineer/CalcIR.java
@@ -0,0 +1,13 @@
+package src.javaEngineer;
+
+import java.util.Scanner;
+
+public class CalcIR {
+    public static void main(String[] args) {
+
+        Scanner data = new Scanner(System.in);
+
+        System.out.println("Insert your ");
+        double salary = data.nextDouble();
+    }
+}

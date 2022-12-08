a0c2ecc60542ab1a3cc2b6912cfeea4278ad0b10
Jefferson <jefferson.moura@hp.com>
2022-12-08 11:39:14 -0300

    counter

---
+++ src/javaEngineer/Counter.java
@@ -0,0 +1,12 @@
+package src.javaEngineer;
+
+public class Counter {
+    public static void main(String[] args) {
+
+        int counter = 0;
+        while(counter <= 10){
+            System.out.println("Number counter is " + counter);
+            counter++;
+        }
+    }
+}

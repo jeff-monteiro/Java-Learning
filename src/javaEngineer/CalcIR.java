e6ebffd5b5dee9e0d10b573ddb63276b48f920d7
Jefferson <jefferson.moura@hp.com>
2022-12-08 11:16:24 -0300

    calculator IR

--- src/javaEngineer/CalcIR.java
+++ src/javaEngineer/CalcIR.java
@@ -10,8 +10,19 @@ public class CalcIR {
         System.out.println("Insert your ");
         double salary = data.nextDouble();
 
-        if(salary > 2000 && salary < 2500){
+        if(salary > 1900.00 && salary <= 2800.00){
+            System.out.println("You must to pay 7.5%");
+            double calcIR = salary - (100 * 0.75);
+            System.out.println("Your salary with dedution is: " + calcIR);
+
+        }else if(salary >= 2801.00 && salary <= 3751.00){
             System.out.println("You must to pay 15%");
+            double calcIR = salary - (100 * 0.15);
+            System.out.println("Your salary with dedution is: " + calcIR);
+        }else{
+            System.out.println("You must to pay 22.5%");
+            double calcIR = salary - (100 * 0.22);
+            System.out.println("Your salary with dedution is: " + calcIR);
         }
     }
 }

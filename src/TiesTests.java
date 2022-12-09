40eb89995cc71f6760845cef84d3991d88acb33a
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-12-08 23:27:59 -0300

    multiplication table

---
+++ src/TiesTests.java
@@ -0,0 +1,14 @@
+package src;
+
+public class TiesTests {
+    public static void main(String[] args) {
+
+        for(int mult=1; mult <= 10; mult++){
+            for(int cont = 0; cont <= 10; cont++){
+                System.out.print(mult * cont);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}

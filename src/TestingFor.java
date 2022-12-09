34de495576ad58f824c7c3c18e8ac9615295f88b
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-12-08 22:53:10 -0300

    using command FOR

---
+++ src/TestingFor.java
@@ -0,0 +1,17 @@
+package src;
+
+public class TestingFor {
+    public static void main(String[] args) {
+        // This are the two ways to apply conditionals.
+
+        for(int i=0; i <= 10; i++){
+            System.out.println("Value of i is: " + i);
+        }
+
+        int cont = 0;
+        while(cont <= 20){
+            System.out.println(cont);
+            cont++;
+        }
+    }
+}

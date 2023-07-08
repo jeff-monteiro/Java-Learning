d9e3b82c12b54191294d288ed6df057123f087a8
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2023-07-08 19:19:50 -0300

    refactor_scanner

--- src/javaEngineer/CalcIR.java
+++ src/javaEngineer/CalcIR.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class CalcIR {
     public static void main(String[] args) {
 
-        Scanner data = new Scanner(System.in);
+        try (Scanner data = new Scanner(System.in)) {
+            System.out.println("Insert your salary");
+            double salary = data.nextDouble();
 
-        System.out.println("Insert your salary");
-        double salary = data.nextDouble();
+            if(salary > 1900.00 && salary <= 2800.00){
+                System.out.println("You must to pay 7.5%");
+                double calcIR = salary - (100 * 0.75);
+                System.out.println("Your salary with dedution is: " + calcIR);
 
-        if(salary > 1900.00 && salary <= 2800.00){
-            System.out.println("You must to pay 7.5%");
-            double calcIR = salary - (100 * 0.75);
-            System.out.println("Your salary with dedution is: " + calcIR);
-
-        }else if(salary >= 2801.00 && salary <= 3751.00){
-            System.out.println("You must to pay 15%");
-            double calcIR = salary - (100 * 0.15);
-            System.out.println("Your salary with dedution is: " + calcIR);
-        }else{
-            System.out.println("You must to pay 22.5%");
-            double calcIR = salary - (100 * 0.225);
-            System.out.println("Your salary with dedution is: " + calcIR);
+            }else if(salary >= 2801.00 && salary <= 3751.00){
+                System.out.println("You must to pay 15%");
+                double calcIR = salary - (100 * 0.15);
+                System.out.println("Your salary with dedution is: " + calcIR);
+            }else{
+                System.out.println("You must to pay 22.5%");
+                double calcIR = salary - (100 * 0.225);
+                System.out.println("Your salary with dedution is: " + calcIR);
+            }
         }
     }
 }

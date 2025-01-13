bf5afb44a802132acab07642ee978e5b8ec7990f
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2025-01-13 15:02:48 -0300

    labs_Java_while

---
+++ src/start/labs/lab11.java
@@ -0,0 +1,22 @@
+package src.start.labs;
+
+import java.util.Scanner;
+
+public class lab11 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numberTest = sc.nextInt();
+
+        int soma = 0;
+        while( numberTest != 0 ){
+            soma += numberTest;
+            numberTest = sc.nextInt();
+
+        }
+        System.out.println(soma);
+
+        sc.close();
+    }
+}
---
+++ src/start/labs/lab12.java
@@ -0,0 +1,19 @@
+package src.start.labs;
+
+import java.util.Scanner;
+
+public class lab12 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int xp = sc.nextInt();
+        int yt = 0;
+
+        while(xp < 7) {
+            xp += 2;
+            yt += 4;
+            System.out.println(xp + " - " + yt);
+
+        }
+    }
+}

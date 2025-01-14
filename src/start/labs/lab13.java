cabb5754454dfcd574a31b23ea35a6eefe5f762f
Jefferson Monteiro <jjmonteiro61@gmail.com>
2025-01-13 21:26:24 -0300

    labs_Java_StringsFmt

---
+++ src/start/labs/lab13.java
@@ -0,0 +1,23 @@
+package src.start.labs;
+
+import java.util.Scanner;
+
+public class lab13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char resp;
+        do{
+            System.out.print("Digite a temperatura em Celsius: ");
+            double temp = sc.nextDouble();
+            double fahrenheit = 9 * temp / 5 + 32;
+
+            System.out.printf("Equivalente a Fahrenheit %.1f%n", fahrenheit);
+            System.out.println("Deseja repetir s/n?");
+            resp = sc.next().charAt(0);
+
+        } while(resp != 'n');
+
+        sc.close();
+    }
+}

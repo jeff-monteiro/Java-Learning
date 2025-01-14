69676c9386abc87bd14dce78228ce12b17c3257e
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2025-01-14 16:58:14 -0300

    fixing_triangle_class

--- src/start/labs/funcs/application/Program.java
+++ src/start/labs/funcs/application/Program.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // Instanciando uma classe Java.
         Triangle x, y;
         x = new Triangle();
         y = new Triangle();
@@ -20,11 +21,9 @@ public class Program {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+        double areaX = x.area();
 
-        p = (y.a + y.b + y.c) / 2;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        double areaY = y.area();
 
         if(areaX > areaY){
             System.out.printf("Larger area: X %.4f%n ", areaX);
--- src/start/labs/funcs/entities/Triangle.java
+++ src/start/labs/funcs/entities/Triangle.java
@@ -4,4 +4,11 @@ public class Triangle {
     public double a;
     public double b;
     public double c;
+
+    // Atribuindo responsabilidade para a devida classe de fazer o cálculo da area.
+    public double area(){
+        double p = (a + b + c) / 2;
+        double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return result;
+    }
 }

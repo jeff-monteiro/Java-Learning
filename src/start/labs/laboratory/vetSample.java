46c6d9d633f11dd0ead05b38d93e3e7ac95e033a
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2025-01-28 21:46:27 -0300

    labs_Java - Vetores

---
+++ src/start/labs/laboratory/vetSample.java
@@ -0,0 +1,26 @@
+package src.start.labs.laboratory;
+
+import java.util.Scanner;
+
+public class vetSample {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+
+        for(int i=0; i<3; i++){
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0;
+        for (int i=0;i<n;i++){
+            sum += vect[i];
+        }
+
+        double average = sum / n;
+        System.out.println(average);
+        sc.close();
+    }
+}

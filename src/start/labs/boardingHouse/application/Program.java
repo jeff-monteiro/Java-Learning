339d7c62e211039b66174a5aa3df18740519a635
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2025-01-31 22:06:33 -0300

    labs_Java - boardingHouseApp

--- src/start/labs/boardingHouse/application/Program.java
+++ src/start/labs/boardingHouse/application/Program.java
@@ -13,22 +13,27 @@ public class Program {
         System.out.print("How many rooms be rented?");
         int roomsQuantity = sc.nextInt();
 
-
-
-
         for(int i = 1; i <= roomsQuantity; i++){
             System.out.println("Tenant #" + i + ":");
-            System.out.println("Name: ");
+            System.out.print("Name: ");
             sc.nextLine();
             String name = sc.nextLine();
-            System.out.println("Email: ");
+            System.out.print("Email: ");
             String email = sc.next();
+            System.out.print("Room: ");
             int roomNumber = sc.nextInt();
 
             tenants[roomNumber] = new Tenants(name, email);
 
         }
+        System.out.println("Busy rooms:");
+        for(int i = 0; i < tenants.length; i++){
+            if(tenants[i] != null){
+                System.out.println(i + ": " + tenants[i]);
+            }
 
+        }
+        sc.close();
     }
 
 }

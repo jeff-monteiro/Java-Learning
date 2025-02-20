fd01cc5e9b6cbeb49866f6d3bdeae9ded42b211c
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2025-02-20 19:20:00 -0300

    labs_Java - matrizes

--- src/start/labs/laboratory/lab18.java
+++ src/start/labs/laboratory/lab18.java
@@ -19,6 +19,17 @@ public class lab18 {
         for(int i = 0; i < n; i++){
             System.out.print(mat[i][i] + " ");
         }
+        System.out.println();
+
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] < 0){
+                    count++;
+                }
+            }
+        }
+        System.out.println("Negative numbers " + count);
 
     }
 }
---
+++ src/start/labs/laboratory/lab19.java
@@ -0,0 +1,17 @@
+package src.start.labs.laboratory;
+
+public class lab19 {
+    public static void main(String[] args) {
+
+        String[] vect = new String[]{"Jefferson", "Karina", "Joelma"};
+        /**
+        for (int i=0; i<vect.length; i++){
+            System.out.println(vect[i]);
+        }
+         */
+
+        for (String obj : vect){
+            System.out.println(obj);
+        }
+    }
+}

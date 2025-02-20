73d243a6da069f24b4296fb41002209296c16463
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2025-02-20 19:45:38 -0300

    labs_Java - matrizes

--- src/start/labs/laboratory/lab18.java
+++ src/start/labs/laboratory/lab18.java
@@ -11,19 +11,19 @@ public class lab18 {
         int[][] mat = new int[n][n];
 
         for(int i = 0; i < mat.length; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < mat[i].length; j++){
                 mat[i][j] = sc.nextInt();
             }
         }
         System.out.println("Diagonal numbers");
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < mat.length; i++){
             System.out.print(mat[i][i] + " ");
         }
         System.out.println();
 
         int count = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        for (int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
                 if(mat[i][j] < 0){
                     count++;
                 }
